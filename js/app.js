c90f31088648a45c196c8638351447025fbe4a0f
lyadnov <roman.lyadnov@gmail.com>
2014-07-15 21:19:01 +1100

    Доделал раздел services

--- js/app.js
+++ js/app.js
@@ -15932,10 +15932,11 @@ $(function() {
 
   $('nav ul a[href^=#]').on('click', function(event) {
     event.preventDefault();
-    var $elementTarget = $(this).attr('href'),
-      $destination = $($elementTarget).offset().top - headerHeight;
+    var elementTarget = $(this).attr('href'),
+        headerHeight = $('.header').height(),
+        destination = $(elementTarget).offset().top - headerHeight;
 
-    $('body, html').animate({scrollTop: $destination}, 300, 'easeInExpo');
+    $('body, html').animate({scrollTop: destination}, 300, 'easeInExpo');
   });
 
   // Collapse menu
@@ -15947,17 +15948,24 @@ $(function() {
 
   // Glide
 
-  var $slider = $('.slider'),
+  var $slider = $('.slider .container'),
       $slideTitle = $('.slide__title'),
-      $slideDesc = $('.slide__desc');
+      $slideDesc = $('.slide__desc'),
+      $slideButtonLeft = $('.slide__button_left'),
+      $slideButtonRight = $('.slide__button_right'),
+      $slideImage = $('.slide img');
 
   $slideTitle.first().addClass('fadeInUp');
-  $slideDesc.first().addClass('fadeInUp');
+  $slideDesc.first().addClass('fadeInUp'),
+  $slideButtonLeft.first().addClass('fadeInLeft'),
+  $slideButtonRight.first().addClass('fadeInRight'),
+  $slideImage.first().addClass('fadeInUp');
 
   // Animation slide title
 
   var titleEffectIn = 'fadeInUp',
-      titleEffectOut = 'fadeOutUp';
+      buttonLeftEffectIn = 'fadeInLeft',
+      buttonRightEffectIn= 'fadeInRight';
 
 
   $slider.glide({
@@ -15972,14 +15980,98 @@ $(function() {
     autoplay: 5000,
     circular: false,
     beforeTransition: function() {
-      $slideTitle.eq(-this.currentSlide).removeClass( titleEffectIn ).addClass( titleEffectOut );
-      $slideDesc.eq(-this.currentSlide).removeClass( titleEffectIn ).addClass( titleEffectOut );
+      $slideTitle.eq(-this.currentSlide).removeClass( titleEffectIn );
+      $slideDesc.eq(-this.currentSlide).removeClass( titleEffectIn );
+      $slideButtonLeft.eq(-this.currentSlide).removeClass( buttonLeftEffectIn );
+      $slideButtonRight.eq(-this.currentSlide).removeClass( buttonRightEffectIn );
+      $slideImage.eq(-this.currentSlide).removeClass( titleEffectIn );
 
     },
     afterTransition: function() {
-      $slideTitle.eq(-this.currentSlide).removeClass( titleEffectOut ).addClass( titleEffectIn );
-      $slideDesc.eq(-this.currentSlide).removeClass( titleEffectOut ).addClass( titleEffectIn );
+      $slideTitle.eq(-this.currentSlide).addClass( titleEffectIn );
+      $slideDesc.eq(-this.currentSlide).addClass( titleEffectIn );
+      $slideButtonLeft.eq(-this.currentSlide).addClass( buttonLeftEffectIn );
+      $slideButtonRight.eq(-this.currentSlide).addClass( buttonRightEffectIn );
+      $slideImage.eq(-this.currentSlide).addClass( titleEffectIn );
     }
   }).data('api_glide');
+
+
+  // Equal height services item
+
+  function setEqualHeight(columns) {
+    var tallestcolumn = 0;
+        
+    columns.removeAttr('style');
+
+    columns.each(function() {
+      var currentHeight = $(this).height();
+
+      if(currentHeight > tallestcolumn) {
+        tallestcolumn  = currentHeight;
+      }
+    });
+
+    columns.height(tallestcolumn);
+  }
+
+  setEqualHeight($(".service"));
+  
+  $(window).resize(function() {
+    setEqualHeight($(".service"));
+  });
+
+  // Animation
+
+  if (Modernizr.cssanimations) {
+    $('.animated').css('opacity', 0);
+  }
+
+  $('.wow').waypoint(function() {
+    var $animated = $(this).find('.animated');
+
+    $animated.each(function() {
+      $(this)
+        .toggleClass( $(this).attr('data-animate') )
+        .css({
+          'animation-delay' : $(this).attr('data-delay') + 'ms'
+        });
+    });
+
+  }, {
+   offset: '75%' // bottom-in-view
+  });
+
+    // Isotope
+
+  $('.portfolio__content').isotope({
+    animationEngine : 'best-available',
+    itemSelector : '.portfolio__thumbnail',
+    layoutMode : 'fitRows'
+  });
+
+
+  // Project Filtering
+
+  projectFilterInit();
+
+  function projectFilterInit() {
+
+    $('.portfolio__filter a').on('click', function(){
+      var selector = $(this).attr('data-filter');
+      var container = $('.portfolio__content');
+
+      container.isotope({
+        filter: selector
+      });
+
+      if (!$(this).hasClass('selected') ) {
+        $(this).parents('.portfolio__filter').find('.selected').removeClass('selected');
+        $(this).addClass('selected');
+      }
+
+      return false;
+    });
+  }
 
 });
